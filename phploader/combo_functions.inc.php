8c7ebc3e917b0aad1998eb54e897392c5f420476
Chad Auld <chadauld@gmail.com>
2009-11-05 11:07:03 -0800

    Removing unused function

--- phploader/combo_functions.inc.php
+++ phploader/combo_functions.inc.php
@@ -22,15 +22,6 @@ function server($includeRequestURI=false)
     return $server;
 }
 
-// Computes the *full* URL of the current page (protocol, server, path, query parameters, etc)
-    function full_url()
-    {
-        $s = empty($_SERVER['HTTPS']) ? '' : ($_SERVER['HTTPS'] == 'on') ? 's' : '';
-        $protocol = substr(strtolower($_SERVER['SERVER_PROTOCOL']), 0, strpos(strtolower($_SERVER['SERVER_PROTOCOL']), '/')) . $s;
-        $port = ($_SERVER['SERVER_PORT'] == '80') ? '' : (":".$_SERVER['SERVER_PORT']);
-        return $protocol . "://" . $_SERVER['HTTP_HOST'] . $port . $_SERVER['REQUEST_URI'];
-    }
-
 function alphaImageLoaderPathCorrection($matches) {
     global $crtResourceBase;
     
