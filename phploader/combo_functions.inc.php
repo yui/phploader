bea64b4d388d34fc3644f496d9ecf767135e1018
Adam Moore <adamoore@yahoo-inc.com>
2010-02-01 13:40:45 -0800

    Re-merge boydjd

--- phploader/combo_functions.inc.php
+++ phploader/combo_functions.inc.php
@@ -9,7 +9,7 @@
 //server(): Computes the base URL of the current page (protocol, server, path)
 //credit: http://code.google.com/p/simple-php-framework/ 
 //(modified version of full_url), license: MIT
-function server($includeRequestURI=false)
+function server($includeRequestUri=false)
 {
     $s = getenv('HTTPS') ? '' : (getenv('HTTPS') ==     'on') ? 's' : '';
     $protocol = substr(
@@ -19,7 +19,7 @@ function server($includeRequestURI=false)
     $port = (getenv('SERVER_PORT') == '80') ? '' : (":".getenv('SERVER_PORT'));
     $server = $protocol . "://" . getenv('HTTP_HOST') . $port;
     
-    if ($includeRequestURI === true) {
+    if ($includeRequestUri === true) {
         $server .= getenv('REQUEST_URI');
     }
     
