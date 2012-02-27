7f39710819aae67b767900822b9100a6bea23563
Damien Pobel <dp@ez.no>
2012-02-27 17:01:42 +0100

    Fixed meta export script to work with version >= 3.4.0

--- scripts/export_modules.js
+++ scripts/export_modules.js
@@ -17,11 +17,16 @@ var YUI = require(path.join(yui3, 'build/yui/yui')).YUI;
 var Y = YUI();
 
 var loader = new Y.Loader({});
+var meta = {
+    skin: loader.skin,
+    moduleInfo: loader.moduleInfo,
+    base: loader.base
+};
 
 var out = path.join(__dirname, '../lib/meta/json_' + Y.version + '.txt');
 var out2 = path.join(__dirname, '../lib/meta/json_aliases_' + Y.version + '.txt');
 
-fs.writeFileSync(out, JSON.stringify(loader.moduleInfo), 'utf8');
+fs.writeFileSync(out, JSON.stringify(meta), 'utf8');
 console.log('JS Loader meta written: ', out);
 if (YUI.Env.aliases) {
     fs.writeFileSync(out2, JSON.stringify(YUI.Env.aliases), 'utf8');
